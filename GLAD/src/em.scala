4ff65cf772dc0c614845ee839f05bde1a50d8a4c
Ethan Petuchowski <ethanp@utexas.edu>
2013-05-14 11:13:44 -0500

    Finished reading in data

--- GLAD/src/em.scala
+++ GLAD/src/em.scala
@@ -1,12 +1,20 @@
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
 /**
  * Based on "em.c"
  */
+
+class Label {
+    var imageIdx  = 0
+    var labelerId = 0
+    var label     = 0
+}
+
 class em {
 
     /* read in from main of file */
-    val labels = Array[Label]()
+    val labels = new ArrayBuffer[Label]()
 
     /* read in from top of file */
     var numLabels   = 0
@@ -17,8 +25,8 @@ class em {
     /* arrays sized according to above */
 
     // set as 1 in his code
-    val priorAlpha = Array[Double]()
-    val priorBeta  = Array[Double]()
+    var priorAlpha = Array[Double]()
+    var priorBeta = Array[Double]()
 
     // from E-step
     val priorProbZ1 = Array[Double]()
@@ -29,27 +37,41 @@ class em {
     val beta  = Array[Double]()
 
     // set as 0.5 by file
-    val priorZ1 = Array[Double]()
+    var priorZ1 = Array[Double]()
 }
 
 object em {
     def main(args: Array[String]) {
         println("hello world")
+
+        /* Read Data */
         val dataLocation = "../../OptimalLabelingRelease1.0.3/data.txt"
         val lines = Source.fromFile(dataLocation).getLines()
         val runIt = new em()
         val something = lines.next().split(" ")
-        runIt.numLabels = something(0).toInt
+
+        // extract metadata from first line
+        runIt.numLabels   = something(0).toInt
         runIt.numLabelers = something(1).toInt
-        runIt.numImages = something(2).toInt
-        runIt.forPriorZ1 = something(3).toDouble
+        runIt.numImages   = something(2).toInt
+        runIt.forPriorZ1  = something(3).toDouble
 
-        val a = 9  // doofus line
+        // initialize priors
+        runIt.priorAlpha = Array.fill[Double](runIt.numLabelers)(1.0)
+        runIt.priorBeta  = Array.fill[Double](runIt.numImages)(1.0)
+        runIt.priorZ1    = Array.fill[Double](runIt.numImages)(runIt.forPriorZ1)
 
-        
+        // read in the data
         for(line <- lines) {
             val lineData = line.split(" ")
-            // more code shall go here
+            val label = new Label()
+            label.imageIdx = lineData(0).toInt
+            label.labelerId = lineData(1).toInt
+            label.label = lineData(2).toInt
+            runIt.labels += label
         }
+
+        /* Run EM */
+        
     }
 }
