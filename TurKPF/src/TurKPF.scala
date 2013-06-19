a39e38cf2b382aa7e4be8b888d613865a30fc677
Ethan Petuchowski <ethanp@utexas.edu>
2013-06-18 23:24:34 -0500

    Compiles, but it's just beginnings of the layout

--- TurKPF/src/TurKPF.scala
+++ TurKPF/src/TurKPF.scala
@@ -17,21 +17,42 @@ import org.apache.commons.math3.distribution.NormalDistribution
  *  Type Info: cmd-T
  */
 
-class ExperimentalPplGenerator extends App
+case class Turker(errorCoeff: Double)
+
+object FirstExperiment extends App
 {
     val utilityFunction = 1000 * (exp(3) - 1) / (exp(1) - 1)
     val initialArtifactQuality = new BetaDistribution(1,9).sample
-    val costRatioImprovementsVsBallots = 3  // "because ballots take less time" (pg. 1172)
-    val difficultyConstant = 0.5
-    val numWorkers = 1000  // these may need to become instances of a class?
-    val workers = new Array[Turker](numWorkers)
-    workers.foreach(println(_.errorCoefficient))
-}
 
-class Turker
-{
-    // TODO: figure out what the stdev of this is supposed to be
-    // I set it to 2 with 0 thought
-    val errorCoefficient = new NormalDistribution(1,2).sample
+    // TODO: figure out a reasonable stdev, I set it to 1 with 0 thought
+    val workerDist = new NormalDistribution(1,1)
+
+    val costRatioImprovementsVsBallots = 3  // "because ballots take less time" (pg. 1172)
+    val ballotCost = 10
+    val improvementCost = ballotCost * costRatioImprovementsVsBallots
+    val difficultyConstant = 0.5
+    val lookaheadDepth = 3
+    val allowance = 400  // the amount of $$ we start out with
+    var allowanceBalance = allowance
+    val numTurkers = 1000
+    val numTrials = 10000  // 10k
+
+    var q = initialArtifactQuality
+    val workers = (1 to 100).map(x => Turker(workerDist.sample)).toArray
+
+    for (i <- numTrials) {
+        // make a decision here.
+    }
+    def improvement_job() = ???
+    def vote_job() = ???
+    def submit_final() = ???
+    def get_utility_of_stopping_voting() = ???
+    def get_utility_of_one_more_ballot() = ???
+    def get_utility_of_improvement_step() = ???
+    def get_utility_of_submitting_final() = ???
+    def update_difficulty() = ???
+    def update_worker_accuracy_estimates() = ???
+    def update_belief_state() = ???
+
 }
 
