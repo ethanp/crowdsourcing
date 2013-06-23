9fbbc1e3470d2f530be97459c64c9ad178483df0
Ethan Petuchowski <ethanp@utexas.edu>
2013-06-23 16:24:45 -0500

    Filled in some deets

--- TurKPF/src/main/scala/edu/utexas/turkpf/TurKPF.scala
+++ TurKPF/src/main/scala/edu/utexas/turkpf/TurKPF.scala
@@ -23,31 +23,51 @@ import scala.util.Random
 case class BallotJob(qstn: Question)
 {
     val ballotCost = .01
-    def get_addnl_ballot(): Boolean = {
+    def utility_of_stopping_voting(): Double = ???
+    def utility_of_voting(): Double = ???
+    def decide_whether_to_vote(): Boolean = utility_of_voting < utility_of_stopping_voting
+    def get_addnl_ballot() {
         /* pay for it */
         qstn.allowanceBalance -= ballotCost
 
         /* random worker gets chosen to vote */
         val worker = Random.shuffle(qstn.workers.toList).head
         worker.generateVote(qstn.difficulty)  // currently just updates 'vote' field
-        worker.vote
+    }
+
+    var votes = List[Boolean]()
+
+    while (decide_whether_to_vote) {
+        votes ::= get_addnl_ballot()
     }
 }
 
 case class ParticleFilter(numParticles: Int, dist: RealDistribution)
 {
     val priorDistribution = dist.sample(numParticles)
-    def updatePrior = ???  // propagate, observe, re-estimate
+    def updatePrior {propagate; observe; re_estimate}
     def propagate = ???
     def observe = ???
     def re_estimate = ???
+    def sample = ???
 }
 
-case class Turker(trueGX: Double)
+case class Turker(trueGX: Double, qstn: Question)
 {
-    val learningRate = 0.05  // maybe there's a better number to use here
-    var estGX: Double = 1  // mean of true distribution (not sure what else to use)
-    val conditionalImprovementGivenTurkerFunction = ParticleFilter(100)
+    val learningRate = 0.05  // perhaps there is a better number to use here
+    var estGX: Double = 1    // was simply set to the mean of the true distribution
+
+    def find_improvementFunctionMean: Double = // [DTC] (eq. 13)
+        qstn.q +
+          0.5 * ((1 - qstn.q) * (accuracy(qstn.difficulty) - 0.5) +
+          qstn.q * (accuracy(qstn.difficulty) - 1))
+
+    val conditionalImprovementGivenTurkerFunction =  // [DTC] § Experiments
+        ParticleFilter(100,
+            new BetaDistribution(
+                10*find_improvementFunctionMean,
+                10*(1-find_improvementFunctionMean)))
+
     var vote = false
 
     def generateVote(d: Double) { vote = random < accuracy(d) } // [DTC] (eq. 3)
@@ -64,27 +84,27 @@ case class Turker(trueGX: Double)
 
 case class Question(trueAnswer: Boolean, q: Double, dC: Double)
 {
-    val allowance = 400  // the amount of $$ we start out with
-    var allowanceBalance = allowance
+    val allowance: Int = 400  // the amount of $$ we start out with
+    var allowanceBalance: Double = allowance
 
-    // TODO: figure out a reasonable stdev
     val workerDist = new NormalDistribution(1,1)
 
     var qPrime = 0.0
     def difficulty = 1 - pow((q - qPrime).abs, dC)
-    def dStar = ???  // [DTC] (below eq. 12)
-    val priorDensityFctn = ParticleFilter(100, new BetaDistribution(1,5)) // random parameters
+    def utility = 1000 * (exp(q) - 1) / (exp(1) - 1)
+    def dStar = ???  // [DTC] (eq. 12)
+    val priorQualityDensityFctn =
+        ParticleFilter(100, new BetaDistribution(1, 9)) // [DTC] § Experimental Setup
 
     /* the 'belief state' */
     val joint_prob_dens_of_q_and_qPrime = ParticleFilter(100, new BetaDistribution(1,5)) // random parameters)
     val numTurkers = 1000
-    val workers = Array.fill(numTurkers)(Turker(workerDist.sample))
+    val workers = Array.fill(numTurkers)(Turker(workerDist.sample, this))
 }
 
 object FirstExperiment
 {
     // what is this?? shouldn't it be a function of q?
-    val utilityFunction = 1000 * (exp(3) - 1) / (exp(1) - 1)
     def initialArtifactQuality = new BetaDistribution(1,9).sample
 
 
@@ -93,11 +113,17 @@ object FirstExperiment
     val lookaheadDepth = 3
     val numTrials = 10000
 
-    val question = Question(trueAnswer=true, initialArtifactQuality, difficultyConstant)
+    val qstn = Question(trueAnswer=true, initialArtifactQuality, difficultyConstant)
 
 
     /* methods I'll probably want */
-    def choose_action() = ???
+    def choose_action() {  // I don't think this is correctly implemented
+        if (decide_whether_to_vote)
+            BallotJob
+        if (utility_of_current_artifact > utility_of_voting &&
+            utility_of_current_artifact > utility_of_improvement_step)
+            submit_final()
+    }
 
     def improvement_job() = ???
 
@@ -105,9 +131,6 @@ object FirstExperiment
 
     def update_posteriors_for_alphas() = ???
 
-    def utility_of_stopping_voting(): Double = ???
-
-    def utility_of_voting(): Double = ???
 
     def utility_of_improvement_step(): Double = ???
 
@@ -115,10 +138,11 @@ object FirstExperiment
 
     def update_belief_state() { ??? }
 
-    /* methods I have written */
-    def utility_of_current_artifact(): Double = 25*question.q  // AI-AAI
+    def submit_final() = sys.exit(0)
 
-    def decide_whether_to_vote(): Boolean = utility_of_voting < utility_of_stopping_voting
+    /* methods I have written */
+    def utility_of_current_artifact(): Double = 25*qstn.q  // AI-AAI
+
 }
 
 object TestStuff extends App
