2aee679a83d212a7ac4514fe002803e8002c2e53
Ethan Petuchowski <ethanp@utexas.edu>
2013-06-23 14:34:40 -0500

    Start of the day

--- TurKPF/src/main/scala/edu/utexas/turkpf/TurKPF.scala
+++ TurKPF/src/main/scala/edu/utexas/turkpf/TurKPF.scala
@@ -18,10 +18,11 @@ import scala.util.Random
  *  Type Info: cmd-T
  */
 
-case class Turker(trueError: Double, var gX /* estimated error */: Double)
+case class Turker(trueGX: Double)
 {
+    var estGX: Double = 1  // mean of true distribution (not sure what else to use)
     def generateVote(d: Double): Boolean = random < accuracy(d)
-    def accuracy(d: Double) = (1/2)*(1+pow(1-d, gX))
+    def accuracy(d: Double) = (1/2)*(1+pow(1-d, estGX))
     def updateGX() = ???
 }
 
@@ -29,10 +30,12 @@ case class Question(trueAnswer: Boolean, q: Double)
 {
     var qPrime = 0.0
     def difficulty = 1 - pow((question.q - qPrime).abs, difficultyConstant)
+    def priorDensityFctn = ???  // this will use a particle filter
 }
 
 object FirstExperiment
 {
+    // what is this?? shouldn't it be a function of q?
     val utilityFunction = 1000 * (exp(3) - 1) / (exp(1) - 1)
     def initialArtifactQuality = new BetaDistribution(1,9).sample
 
@@ -50,7 +53,7 @@ object FirstExperiment
 
     val question = Question(trueAnswer=true, initialArtifactQuality)
 
-    val workers = Array.fill(numTurkers)(Turker(workerDist.sample, workerDist.sample))
+    val workers = Array.fill(numTurkers)(Turker(workerDist.sample))
 
     /* the flowchart in method-names */
     def choose_action() = ???
@@ -61,33 +64,31 @@ object FirstExperiment
 
     def estimate_prior_for_alphaPrime() = ???
 
-    def decide_whether_to_vote() = ???
+    def update_posteriors_for_alphas() = ???
+
+    /* other methods I'll probably want */
+    def utility_of_stopping_voting(): Double = ???
+
+    def utility_of_voting(): Double = ???
+
+    def utility_of_improvement_step(): Double = ???
+
+    def re_estimate_worker_accuracy(workerIndex: Int) { ??? }
+
+    def update_belief_state() { ??? }
+
+    /* methods I have written */
+    def utility_submitting_final(): Double = 25*question.q  // AI-AAI
 
     def ballot_job(): Boolean = {
         /* pay for it */
         allowanceBalance -= ballotCost
 
-        /* random worker gets chosen */
+        /* random worker gets chosen to vote */
         val worker = Random.shuffle(workers.toList).head
         worker.generateVote(question.difficulty)
     }
-    def update_posteriors_for_alphas() = ???
-
-    /* other methods I'll probably want */
-    def get_utility_of_stopping_voting(): Double = ???
-
-    def get_utility_of_one_more_ballot(): Double = ???
-
-    def get_utility_of_improvement_step(): Double = ???
-
-    def get_utility_of_submitting_final(): Double = ???
-
-    def update_worker_accuracy_estimate(workerIndex: Int) { ??? }
-
-    def update_belief_state() { ??? }
-
-    /* methods I have written */
-    def get_utility_submitting_final(): Double = 25*question.q
+    def decide_whether_to_vote(): Boolean = utility_voting < utility_stop_voting
 }
 
 object TestStuff extends App
