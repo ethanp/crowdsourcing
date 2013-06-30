ce2ba4eec42c1d8862da73f69442a8c6f06fa3ee
Ethan Petuchowski <ethanp@utexas.edu>
2013-06-30 18:51:27 -0500

    Completely Re-Jiggered: All workers are represented by One instance of Workers. FirstExperiment is just an object that sets the parameters for the Questions and runs a bunch of Questions. The actual decisions are made within the Question instance.
    Saved the old version in the base SummerReading folder
    outside of the Git repo.

--- TurKPF/src/main/scala/edu/utexas/turkpf/TurKPF.scala
+++ TurKPF/src/main/scala/edu/utexas/turkpf/TurKPF.scala
@@ -27,10 +27,7 @@ case class BallotJob(qstn: Question)
         /* pay for it */
         qstn.allowanceBalance -= ballotCost
 
-        /* random worker gets chosen to vote */
-        val worker = Random.shuffle(qstn.workers.toList).head
-        worker.generateVote(qstn.difficulty)  // currently just updates 'vote' field
-        worker.vote
+        qstn.WORKERS.generateVote(qstn.difficulty)
     }
 
     var votes = List[Boolean]()
@@ -38,6 +35,7 @@ case class BallotJob(qstn: Question)
     while (decide_whether_to_vote) {
         votes ::= get_addnl_ballot()
     }
+    qstn.WORKERS.updateGX(votes)
 }
 
 abstract class ParticleFilter(numParticles: Int, dist: RealDistribution)
@@ -68,32 +66,37 @@ case class QualityDistribution(numParticles: Int, dist: RealDistribution)
     def sample {}
 }
 
-case class Worker(trueGX: Double, qstn: Question)
+/* I am modelling all workers with just one instance
+ *  instead of having one worker-independent model
+ *  and multiple worker-dependent models
+ * in order to simply my life a bit
+ */
+case class Workers(trueGX: Double, qstn: Question)
 {
-    val learningRate = 0.05  // perhaps there is a better number to use here
+    val learningRate = 0.05
     var estGX: Double = 1    // set to the mean of the true distribution
 
     def find_improvementFunctionMean: Double = // [DTC] (eq. 13)
-        qstn.q +
-          0.5 * ((1 - qstn.q) * (accuracy(qstn.difficulty) - 0.5) +
-          qstn.q * (accuracy(qstn.difficulty) - 1))
+        qstn.q
+          + 0.5 * ((1 - qstn.q) * (accuracy(qstn.difficulty) - 0.5)
+          + qstn.q * (accuracy(qstn.difficulty) - 1))
 
-    val workerFctn =  // [DTC] § Experiments
+    def workerFctn =  // [DTC] § Experimental Setup
             new BetaDistribution(
-                10*find_improvementFunctionMean,
-                10*(1-find_improvementFunctionMean)))
+                10 * find_improvementFunctionMean,
+                10 * (1 - find_improvementFunctionMean))
 
-    var vote = false
+    def generateVote(d: Double): Boolean = random < accuracy(d) // [DTC] (eq. 3)
 
-    def generateVote(d: Double) { vote = random < accuracy(d) } // [DTC] (eq. 3)
+    def accuracy(d: Double) = 0.5 * (1 + pow(1-d, estGX))
 
-    def accuracy(d: Double) = (1/2)*(1+pow(1-d, estGX))
-
-    def updateGX(qstn: Question) = {    // [DTC] (below eq. 12)
-        if(vote == qstn.trueAnswer)
-            estGX -= qstn.difficulty * learningRate
-        else
-            estGX += (1 - qstn.difficulty) * learningRate
+    def updateGX(votes: List[Boolean]) {    // [DTC] (below eq. 12)
+        for (vote <- votes) {
+            if(vote == qstn.trueAnswer)
+                estGX -= qstn.difficulty * learningRate
+            else
+                estGX += (1 - qstn.difficulty) * learningRate
+        }
     }
 
     def prob_true_given_qS(): Double = {
@@ -106,68 +109,75 @@ case class Worker(trueGX: Double, qstn: Question)
     def mean_f_QP_giv_qp(): Double = ??? // find avg loc of particles in associated Particle Filter
 }
 
-case class Question(trueAnswer: Boolean, q: Double, dC: Double)
+case class Question(trueAnswer: Boolean)
 {
-    var allowanceBalance: Double = 400  // the amount of $$ we start out with
+    /* this means I can choose all new parameters by replacing this line with
+     *  import SecondExperiment._  and so on  */
+    import FirstExperiment._
 
-    /* [DTC] gmX "follow a bell shaped distribution" "average error coefficient gm=1" */
-    val workerDist = new NormalDistribution(1,1)
-
+    var allowanceBalance = INITIAL_ALLOWANCE
+    var q = INITIAL_QUALITY
     var qPrime = 0.0
-    def difficulty = 1 - pow((q - qPrime).abs, dC)      // [DTC] (eq. 2)
-    def utility = 1000 * (exp(q) - 1) / (exp(1) - 1)    // [DTC] § Experimental Setup
+    val WORKERS = Workers(WORKER_DIST.sample, QUESTION)
+
+
+    def difficulty = 1 - pow((q - qPrime).abs, DIFFICULTY_CONSTANT)      // [DTC] (eq. 2)
+    def estimated_artifact_utility = 1000 * (exp(q) - 1) / (exp(1) - 1)    // [DTC] § Experimental Setup
     def dStar = ???                                     // [DTC] (eq. 12)
+    def submit_final() = {
+        println("Final Utility: " + estimated_artifact_utility)
+        sys.exit(0)
+    }
+    def choose_action() {
+        if (estimated_artifact_utility > utility_of_ballot_job
+         && estimated_artifact_utility > utility_of_improvement_step)
+            submit_final()
+        else if (utility_of_ballot_job > utility_of_improvement_step)
+            BallotJob
+        else
+            improvement_job()
+    }
 
     val priorQualityDensityFctn =
         QualityDistribution(100, new BetaDistribution(1, 9)) // [DTC] § Experimental Setup
 
-    val belief_state = JointProbDensOfQAndQPrime(100, new BetaDistribution(1,5)) // random parameters
-    val numTurkers = 1000
-    val workers = Array.fill(numTurkers)(Worker(workerDist.sample, this))
-}
 
-object FirstExperiment
-{
-    // what is this?? shouldn't it be a function of q?
-    def initialArtifactQuality = new BetaDistribution(1,9).sample
-
-    val improvementCost = .05
-    val difficultyConstant = 0.5
-    val lookaheadDepth = 3
-    val numTrials = 10000
-
-    val qstn = Question(trueAnswer=true, initialArtifactQuality, difficultyConstant)
-
-    /* methods I'll probably want */
-    def choose_action() {  // I don't think this is correctly implemented
-        if (utility_of_current_artifact > utility_of_ballot_job &&
-          utility_of_current_artifact > utility_of_improvement_step) {
-            submit_final()
-        } else if (utility_of_ballot_job > utility_of_improvement_step) {
-            BallotJob
-        } else {
-            improvement_job()
-        }
-    }
-
+    /******* UNIMPLEMENTED **********/
     def improvement_job() = ???
 
     def estimate_prior_for_alphaPrime() = ???
 
     def update_posteriors_for_alphas() = ???
 
-    def utility_of_improvement_step(): Double = ???
+    def utility_of_improvement_step: Double = ???
 
-    def utility_of_ballot_job(): Double = ???
+    def utility_of_ballot_job: Double = ???
 
     def re_estimate_worker_accuracy(workerIndex: Int) { ??? }
 
     def update_belief_state() { ??? }
+}
 
-    def submit_final() = sys.exit(0)
+object FirstExperiment
+{
+    /* [DTC] gmX "follow a bell shaped distribution" "average error coefficient gm=1" */
+    val WORKER_DIST = new NormalDistribution(1,1)
 
-    /* methods I have written */
-    def utility_of_current_artifact(): Double = 25*qstn.q  // AI-AAI
+    /* this is a method so that it generates a new initial quality every time a question starts */
+    def INITIAL_QUALITY = new BetaDistribution(1,9).sample
+
+    val IMPROVEMENT_COST = .05
+    val DIFFICULTY_CONSTANT = 0.5
+    val LOOKAHEAD_DEPTH = 3
+    val NUM_QUESTIONS = 10000
+    val INITIAL_ALLOWANCE: Double = 400
+
+    /* I suppose one reason to make question a class, and not just bring it all
+     * into this Experiment class, is so that MANY Questions can be run Per Experiment
+     *  I'ma start with just question though, and try and get that working first
+     */
+    val QUESTION = Question(trueAnswer=true)
+
 }
 
 object TestStuff extends App
